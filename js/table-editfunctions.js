c50b7e72a5c9ff4a150548a943d83a479c4a5731
Leo Bezerra <leosoares.bezerra2017@gmail.com>
2020-01-30 10:58:44 -0300

    Modal de acesso

--- js/table-editfunctions.js
+++ js/table-editfunctions.js
@@ -8,12 +8,58 @@ class pessoa {
         this.RG = RG;
         this.Inst = Inst;
     }
+
 }
+
+class password {
+
+
+    constructor(Pwrd) {
+
+        this.cod = Pwrd;
+
+    }
+
+
+
+    setpwrd(newPwrd) {
+
+        this.cod = String(newPwrd);
+
+    }
+
+
+}
+
+class Modal {
+
+    constructor(ID) {
+
+        this.modalID = ID;
+    }
+
+    show() {
+
+        $('#' + this.modalID).modal('show');
+
+    }
+
+    hide() {
+
+        $('#' + this.modalID).modal('hide');
+
+    }
+
+
+
+
+}
+
 /* Adiciona Linha na tabela */
 function add_row() {
 
-
-    $('#cadastromodal').modal('show');
+    var cadastro = new Modal('cadastromodal');
+    cadastro.show();
     document.getElementById('Modalname').value = "";
     document.getElementById('ModalRG').value = "";
     document.getElementById('Modalinst').value = "";
@@ -28,12 +74,13 @@ function editspace() {
 }
 /*salvar dados na tabela*/
 function SavetoTable() {
+    var cadastro = new Modal('cadastromodal');
     var N = document.getElementById('Modalname').value;
     var ID = document.getElementById('ModalRG').value;
     var I = document.getElementById('Modalinst').value;
     if (N != "" && ID && ID != "" && I != "") {
         var Pessoa = new pessoa(N, ID, I);
-        $(".tabledata").append('<tr class="fade show"id=item' + cod + '> <td onclick="editspace()">' + Pessoa.Nome + '</td> <td onclick="editspace()">' + Pessoa.RG + '</td> <td onclick="editspace()">' + Pessoa.Inst +'</td> <td> <button type="button" onclick="dell_row(item'+cod+')" class="btn btn-danger">X</button> </td> </tr > ');
+        $(".tabledata").append('<tr class="fade show"id=item' + cod + '> <td onclick="editspace()">' + Pessoa.Nome + '</td> <td onclick="editspace()">' + Pessoa.RG + '</td> <td onclick="editspace()">' + Pessoa.Inst + '</td> <td> <button type="button" onclick="dell_row(item' + cod + ')" class="btn btn-danger">X</button> </td> </tr > ');
         console.log("Adding_Row");
         cod = cod + 1;
 
@@ -42,7 +89,7 @@ function SavetoTable() {
 
 
     }
-    $('#cadastromodal').modal('hide');
+    cadastro.hide();
     setTimeout(clearalert, 4000);
 
 }
@@ -53,16 +100,29 @@ function clearalert() {
     var alertsx = $(".alert-success").alert('close');
 }
 
-function dell_row(COD){
+function dell_row(COD) {
     COD.parentNode.removeChild(COD);
-   console.log(COD);
+    console.log(COD);
 }
 
 
-function savearq(){
+function savearq() {
 
- 
- console.log("Saving...");
- 
 
+    console.log("Saving...");
+
+
+}
+
+function SetPass() {
+
+
+    var cadastro = new Modal('accessmodal');
+    
+    var COD = new password(document.getElementById("codinput").value);
+
+    COD.setpwrd(COD.cod);
+
+    console.log("The Work Password is: " + COD.cod);
+    cadastro.hide();
 }
